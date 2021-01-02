208317586e762a5dc65fba902582105608e06d1e
Eduardo Borba <eduardo@jobscore.com>
2021-01-02 13:08:08 -0300

    add support for todo item creation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,21 @@ class TodosController < ApplicationController
   def index
     render json: Todo.all
   end
+
+  def create
+    todo = Todo.new(todo_params)
+
+    if todo.valid?
+      todo.save!
+      render json: todo
+    else
+      render json: { error_messages: todo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.permit(:description)
+  end
 end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -4,8 +4,31 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
   test '#index' do
     get '/todos'
 
+    assert_response :success
     response_json = JSON.parse(@response.body)
 
     assert_equal 2, response_json.count
+  end
+
+  test '#create' do
+    assert_difference -> { Todo.count }, +1 do
+      post '/todos', params: { description: 'Add tests to your rails app' }
+    end
+
+    assert_response :success
+    response_json = JSON.parse(@response.body)
+
+    assert_equal 'Add tests to your rails app', response_json['description']
+  end
+
+  test '#create with validation errors' do
+    assert_no_difference -> { Todo.count } do
+      post '/todos', params: { description: '' }
+    end
+
+    assert_response :unprocessable_entity
+    response_json = JSON.parse(@response.body)
+
+    assert_equal "Description can't be blank", response_json['error_messages'][0]
   end
 end
